7b14625c927714aee82d699566f9254abad11570
Intrepid Panda <jerick999@gmail.com>
2018-12-06 08:14:28 -0700

    Add it statements to post locations, get location, and post location describe blocks

--- test/server.test.js
+++ test/server.test.js
@@ -7,7 +7,7 @@ chai.use(chaiHTTP);
 
 describe('API Routes', () => {
   describe('GET /api/v1/locations', () => {
-    it('should return a status of 200', () => {
+    it('should respond with a status of 200', () => {
 
     });
 
@@ -17,15 +17,47 @@ describe('API Routes', () => {
   });
 
   describe('POST /api/v1/locations', () => {
-    // adds a location with required name, abbreviation, and count;
+    it('should respond with a status of 201 if request body is complete', () => {
+
+    });
+
+    it('should add a new location to the database if request body is complete', () => {
+
+    });
+
+    it('should respond with a status of 422 if the request body is incomplete, with instructions to make a complete request', () => {
+
+    });
+
+    it('should respond with a status of 409 if the location already exists, with instructions to do a PATCH instead', () => {
+
+    });
   });
 
   describe('GET /api/v1/locations/:id', () => {
-    // returns one location with all associated bees;
+    it('should respond with a status of 200', () => {
+
+    });
+
+    it('should return a json object, with the location as the key and an array of all associated bees, with each bee being an object with the bee id as the key and the name of the species as the value', () => {
+
+    });
   });
 
   describe('POST /api/v1/locations/:id', () => {
-    // adds a bee to this location by adding it to the bee table with required location id, name, description, and bee fact;
+    it('should respond with a status of 201 if request body is complete', () => {
+
+    });
+
+    it('should add a new bee to the bees database if the request body is complete', () => {
+
+    });
+
+    it('should respond with a status of 422 if the request body is incomplete, with instructions to make a complete request', () => {
+
+    });
+
+    it('should respond with a status of 409 if the bee already exists, with instructions to do a PATCH instead');
   });
 
   describe('PATCH /api/v1/locations/:id', () => {
