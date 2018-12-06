775934f2d0c59833cfaefd8c8e2a2798d5df1448
Nick Dambrosio <ndambro303@gmail.com>
2018-12-06 13:39:26 -0700

    pass 200 status test location id

--- server.js
+++ server.js
@@ -5,20 +5,26 @@ app.set("port", process.env.PORT || 3000);
 
 app.use(express.json());
 
-app.get('/api/v1/locations', (request, response) => {
+app.get("/api/v1/locations", (request, response) => {
   const testArray = [1, 2, 3, 4, 5];
   response.status(200).json(testArray);
 });
 
-app.post('/api/v1/locations', (request, response) => {
+app.get("/api/v1/locations/:id", (request, response) => {
+  response.status(200).send();
+});
+
+app.post("/api/v1/locations", (request, response) => {
   const location = request.body;
 
   if (location.name && location.abbr && location.count) {
-    response.status(201).send({ message: `Location ${location.name} added.`});
+    response.status(201).send({ message: `Location ${location.name} added.` });
   } else {
-    response.status(422).send({ message: 'Your request body was not correct. Please send the following format: {name: <String>, abbr: <String>, count: <Integer>}'});
+    response.status(422).send({
+      message:
+        "Your request body was not correct. Please send the following format: {name: <String>, abbr: <String>, count: <Integer>}"
+    });
   }
-
 });
 
 app.listen(app.get("port"), () => {
