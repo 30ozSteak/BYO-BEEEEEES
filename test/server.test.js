b76a5ecd15aed08bed439fce7afbbc14396d3da9
Intrepid Panda <jerick999@gmail.com>
2018-12-06 07:55:18 -0700

    Flesh out server tests with full endpoints and desired functionality

--- test/server.test.js
+++ test/server.test.js
@@ -6,19 +6,31 @@ const app = require('../server.js');
 chai.use(chaiHTTP);
 
 describe('API Routes', () => {
-  describe('/api/v1/locations', () => {
-
+  describe('GET /api/v1/locations', () => {
+    // returns all locations with bee species count;
   });
 
-  describe('/api/v1/locations/:id', () => {
-
+  describe('POST /api/v1/locations', () => {
+    // adds a location with required name, abbreviation, and count;
   });
 
-  describe('/api/v1/locations/:id/bees' () => {
-
+  describe('GET /api/v1/locations/:id', () => {
+    // returns one location with all associated bees;
   });
 
-  describe('/api/v1/locations/:location_id/bees/:bee_id', () => {
+  describe('POST /api/v1/locations/:id', () => {
+    // adds a bee to this location by adding it to the bee table with required location id, name, description, and bee fact;
+  });
 
+  describe('PATCH /api/v1/locations/:id', () => {
+    // updates location with any new info;
+  });
+
+  describe('GET /api/v1/bees/:bee_id', () => {
+    // returns the bee description and bee fact about this bee;
+  });
+
+  describe('PATCH /api/v1/bees/:bee_id', () => {
+    // updates bee with any new info;
   });
 });
