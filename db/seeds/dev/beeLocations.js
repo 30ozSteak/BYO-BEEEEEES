8b80a1246f6062485f43ecdd99c536eaa80536d1
Nick Dambrosio <ndambro303@gmail.com>
2018-12-08 10:59:08 -0700

    add bee locations object

---
+++ db/seeds/dev/beeLocations.js
@@ -0,0 +1,73 @@
+let beeLocations = [
+  {
+    Angola: [
+      "Euaspis abdominalis",
+      "Hylaeus gabonicus",
+      "Lipotriches notabilis",
+      "Nomia amabilis",
+      "Thyreus abyssinicus"
+    ]
+  },
+  {
+    Haiti: [
+      "Agapostemon centratus",
+      "Augochlora magnifica",
+      "Coelioxys vigilans",
+      "Megachile lanata",
+      "Xeromelecta haitensis"
+    ]
+  },
+  {
+    Iceland: [
+      "Bombus hortorum",
+      "Bombus hypnorum",
+      "Bombus jonellus",
+      "Bombus lucorum",
+      "Bombus pascuorum"
+    ]
+  },
+  {
+    Namibia: [
+      "Afranthidium abdominale",
+      "Anthophora labrosa",
+      "Epeolus amabilis",
+      "Megachile gobabebensis",
+      "Patellapis abnormis"
+    ]
+  },
+  {
+    SanMarino: [
+      "Andrena quintilis",
+      "Apis mellifera",
+      "Bombus terrestris",
+      "Megachile trigonaspis"
+    ]
+  },
+  {
+    Swaziland: [
+      "Amegilla capensis",
+      "Apis mellifera",
+      "Lipotriches cribrosa",
+      "Megachile mossambica",
+      "Thyreus delumbatus"
+    ]
+  },
+  {
+    Trinidad_and_Tobago: [
+      "Augochlora erubescens",
+      "Augochloropsis trinitatis",
+      "Cephalotrigona capitata",
+      "Plebeia tobagoensis",
+      "Trigona amalthea"
+    ]
+  },
+  {
+    Vanuatu: [
+      "Lasioglossum ounuense",
+      "Lasioglossum zingowli",
+      "Megachile diligens",
+      "Megachile serricauda",
+      "Palaeorhiza maluae"
+    ]
+  }
+];
---
+++ db/seeds/dev/locations.js
@@ -0,0 +1,42 @@
+let countries = [
+  {
+    name: "Angola",
+    abbr: "AO",
+    count: 127
+  },
+  {
+    name: "Haiti",
+    abbr: "HA",
+    count: 34
+  },
+  {
+    name: "Iceland",
+    abbr: "IC",
+    count: 5
+  },
+  {
+    name: "Namibia",
+    abbr: "WA",
+    count: 354
+  },
+  {
+    name: "San Marino",
+    abbr: "SM",
+    count: 4
+  },
+  {
+    name: "Swaziland",
+    abbr: "WZ",
+    count: 15
+  },
+  {
+    name: "Trinidad and Tobago",
+    abbr: "TD",
+    count: 111
+  },
+  {
+    name: "Vanuatu",
+    abbr: "NH",
+    count: 14
+  }
+];
