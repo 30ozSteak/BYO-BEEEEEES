6405dc2e3b58a4ffbbdb84f497e433b339fa259b
Intrepid Panda <jerick999@gmail.com>
2018-12-13 08:40:44 -0700

    Define environment as solely testing for test suite

--- test/server.test.js
+++ test/server.test.js
@@ -2,7 +2,7 @@ const chai = require("chai");
 const expect = chai.expect;
 const chaiHTTP = require("chai-http");
 const app = require("../server.js");
-const environment = process.env.NODE_ENV || "testing";
+const environment = "testing";
 const configuration = require("../knexfile")[environment];
 const database = require("knex")(configuration);
 
